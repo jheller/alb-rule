2fb1ab3784f05253aac40b8f057c0a635bc85eb7
John Heller <joheller@deloitte.com.au>
2018-09-19 15:05:26 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # alb-rule
-A CloudFormation custom resource for creating ALB redirection rules
+A CloudFormation custom resource for creating ALB redirection rules.
+
+At the time this was created, AWS had introduced redirect rules on ALBs but
+had not yet made them available in CloudFormation. A new version of the botocore Python library did support it however.
+
+Here is a how to create a Lambda-backed CloudFormation custom resource that can be used to implement redirect rules.
+
+## Lambda Function
+This is a Python 2.7 function. At the time of writing the version of botocore available in the default AWS Lambda environment was not the newest. A newer version had to be included in the zip file in order to use the new redirect functionality.
+
+To build the Lambda zip file:
+
+    cd lambda
+    pip install -r requirements.txt -t .
+    zip -r /tmp/AlbListenerRule .
+
+Copy /tmp/AlbListenerRule.zip to your S3 bucket.
+
+## CloudFormation Example
+The provided CloudFormation template creates :-
+
+* The Lambda function with and appropriate IAM role.
+* An ALB with HTTP and HTTPS listeners
+* A default target group
+* Some example redirect rules for the ALB
+  * Redirect to HTTP to HTTPS
+  * Redirect the /images path to a CloudFront distribution
+
+A full implementation will require something behind the target group such as EC2 instances or an Auto-Scaling Group.
---
+++ lambda/alb_rule.py
@@ -0,0 +1,99 @@
+import botocore
+import boto3
+from botocore.exceptions import ClientError, ParamValidationError
+import sys
+import os
+import urllib2
+import json
+
+def lambda_handler(event, context):
+    print("Lambda Event: " + json.dumps(event))
+
+    responseData = {}
+
+    region = os.environ['AWS_REGION']
+    event_type = event['RequestType']
+
+    ListenerArn = event['ResourceProperties']['ListenerArn']
+    Conditions = event['ResourceProperties']['Conditions']
+    Actions = event['ResourceProperties']['Actions']
+    Priority = int(event['ResourceProperties']['Priority'])
+
+    try:
+        session = botocore.session.get_session()
+        alb = session.create_client('elbv2', region_name=region)
+
+        if event_type == 'Create':
+            _create_rule(alb, ListenerArn, Conditions, Priority, Actions)
+            responseStatus = 'SUCCESS'
+
+        elif event_type == 'Delete':
+            _delete_rule(alb, ListenerArn, Priority)
+            responseStatus = 'SUCCESS'
+
+        elif event_type == 'Update':
+            OldPriority = int(event['OldResourceProperties']['Priority'])
+            _delete_rule(alb, ListenerArn, OldPriority)
+            _create_rule(alb, ListenerArn, Conditions, Priority, Actions)
+            responseStatus = 'SUCCESS'
+
+    except ClientError as e:
+        print "Boto ClientError: %s" % e
+        responseStatus = 'FAILED'
+    except ParamValidationError as e:
+        print "Boto ParamValidationError: %s" % e
+        responseStatus = 'FAILED'
+    except TypeError as e:
+        print "TypeError: %s" % e
+        responseStatus = 'FAILED'
+    except NameError as e:
+        print "NameError: %s" % e
+        responseStatus = 'FAILED'
+    except AttributeError as e:
+        print "AttributeError: %s" % e
+        responseStatus = 'FAILED'
+    except:
+        print("Error:", sys.exc_info()[0])
+        responseStatus = 'FAILED'
+
+    _sendResponse(event, context, responseStatus, responseData)
+
+def _create_rule(alb, ListenerArn, Conditions, Priority, Actions):
+    print('_create_rule' + ListenerArn)
+    response = alb.create_rule(
+        ListenerArn=ListenerArn,
+        Conditions=Conditions,
+        Priority=Priority,
+        Actions=Actions
+    )
+
+def _delete_rule(alb, ListenerArn, Priority):
+    print('_delete_rule with Priority: %d' % Priority)
+    response = alb.describe_rules(ListenerArn=ListenerArn)
+    print("Found rules: " + json.dumps(response))
+    for rule in response['Rules']:
+        if rule['IsDefault']:
+            continue
+        if int(rule['Priority']) == Priority:
+            RuleArn = rule['RuleArn']
+            print("Deleting rule: " + json.dumps(rule))
+            response=alb.delete_rule(RuleArn=RuleArn)
+            break
+
+def _sendResponse(event, context, responseStatus, responseData):
+    print("responseStatus: " + responseStatus)
+    print("responseData: " + json.dumps(responseData))
+    data = json.dumps({
+        'Status': responseStatus,
+        'Reason': 'See the details in CloudWatch Log Stream: ' + context.log_stream_name,
+        'PhysicalResourceId': context.log_stream_name,
+        'StackId': event['StackId'],
+        'RequestId': event['RequestId'],
+        'LogicalResourceId': event['LogicalResourceId'],
+        'Data': responseData
+    })
+    opener = urllib2.build_opener(urllib2.HTTPHandler)
+    request = urllib2.Request(url=event['ResponseURL'], data=data)
+    request.add_header('Content-Type', '')
+    request.get_method = lambda: 'PUT'
+    url = opener.open(request)
